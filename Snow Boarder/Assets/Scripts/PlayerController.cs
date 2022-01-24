9de8ac37486269b66c6ba6bc4c5fa420a530bcee
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-24 19:05:51 +0300

    Snow Boarder

---
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    [SerializeField] float torqueAmount = 1f;
+
+    void Start()
+    {
+     rb2d = GetComponent<Rigidbody2D>();  
+    } 
+    void Update()
+    {
+        Torque();
+    }
+    void Torque()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
